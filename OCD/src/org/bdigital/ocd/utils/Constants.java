8b331e4f7a72cbaa081ece7319abb0936461080a
jordidigital <jroda@ce.bdigital.org>
2014-03-25 09:41:15 +0100

    Fixed 24h time format

--- OCD/src/org/bdigital/ocd/utils/Constants.java
+++ OCD/src/org/bdigital/ocd/utils/Constants.java
@@ -6,11 +6,11 @@ public final class Constants {
 	public static final String HTML_FIELDTYPE_SMALL="2";
 
 	public static final String FORMAT_DATE_WS = "yyyy-MM-dd";
-	public static final String FORMAT_DATEHOUR_WS = "yyyy-MM-dd hh:mm";
+	public static final String FORMAT_DATEHOUR_WS = "yyyy-MM-dd HH:mm";
 
 	public static final String FORMAT_DATE_WEB = "dd/MM/yyyy";
 	public static final String FORMAT_MONTH_WEB = "MMM";
 	public static final String FORMAT_YEAR_WEB = "yyyy";
 	public static final String FORMAT_DAY_WEB = "dd";
-	public static final String FORMAT_DATEHOUR_WEB = "dd/MM/yyyy hh:mm";
+	public static final String FORMAT_DATEHOUR_WEB = "dd/MM/yyyy HH:mm";
 }
